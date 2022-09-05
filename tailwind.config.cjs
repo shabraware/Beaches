9d915c9dfd8447b3b3161a92fc0f4625d8c64e99
YousefElshabrawy <yousefatefelshabrawy@gmail.com>
2022-09-05 15:45:44 +0200

    setup: Initialize the project setup
    Creating the Project using Vite which is the next gen. frontend tool for
    development enviroments
    
    Adding the TailwindCSS and Making some configs
    
    Cleanup some unnecessary files

---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#00040f",
+        secondary: "#00f6ff",
+        dimWhite: "rgba(255, 255, 255, 0.7)",
+        dimBlue: "rgba(9, 151, 124, 0.1)",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+    },
+  },
+  plugins: [],
+};
