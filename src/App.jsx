93b1653747e65c0c99253eb1848b896da6634ebf
YousefElshabrawy <yousefatefelshabrawy@gmail.com>
2022-09-06 16:59:01 +0200

    feat: Add carousel component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from './components/Hero';
 import Destinations from './components/Destinations';
 import Search from './components/Search';
 import Selections from './components/Selections';
+import Carousel from './components/Carousel';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Destinations />
       <Search />
       <Selections />
+      <Carousel />
     </>
   );
 };
---
+++ src/components/Carousel.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+
+const CAROUSEL_DATA = [
+  {
+    url: 'https://images.unsplash.com/photo-1426604966848-d7adac402bff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1447752875215-b2761acb3c5d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+  },
+  {
+    url: 'https://images.unsplash.com/photo-1472214103451-9374bd1c798e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+  },
+];
+
+const Carousel = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const incrementIndex = () => {
+    console.log(currentIndex);
+    setCurrentIndex((currentIndex) => {
+      return (currentIndex + 1) % CAROUSEL_DATA.length;
+    });
+    console.log(currentIndex);
+  };
+  const decrementIndex = () => {
+    console.log(currentIndex);
+    setCurrentIndex((currentIndex) => {
+      return currentIndex === 0 ? CAROUSEL_DATA.length - 1 : currentIndex - 1;
+    });
+    console.log(currentIndex);
+  };
+  return (
+    <section className='container px-4 my-12 h-3/4 relative'>
+      <img src={CAROUSEL_DATA[currentIndex].url} className='rounded-md' />
+      <div
+        onClick={decrementIndex}
+        className='absolute left-8 top-1/2 -translate-y-1/2 bg-white rounded p-1 cursor-pointer'
+      >
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          strokeWidth={1.5}
+          stroke='currentColor'
+          className='w-6 h-6'
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            d='M15.75 19.5L8.25 12l7.5-7.5'
+          />
+        </svg>
+      </div>
+
+      <div
+        onClick={incrementIndex}
+        className='absolute right-8 top-1/2 -translate-y-1/2 bg-white rounded p-1 cursor-pointer'
+      >
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          strokeWidth={1.5}
+          stroke='currentColor'
+          className='w-6 h-6'
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            d='M8.25 4.5l7.5 7.5-7.5 7.5'
+          />
+        </svg>
+      </div>
+    </section>
+  );
+};
+
+export default Carousel;
